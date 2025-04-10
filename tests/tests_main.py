671c8306638f60bab1b45710868ff31a50fb2422
MuRodri <murilorodri@hotmail.com>
2025-04-09 23:19:15 -0300

    test CI workflow

---
+++ tests/tests_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from env import main  # Import your FastAPI app
+
+client = TestClient(main)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"greeting": "Teste 1"}
